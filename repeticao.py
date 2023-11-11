f6ab5aa859542e447ddc5617e80e4c82cdc28f2f
Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>
2023-11-11 11:59:08 -0300

    Update repeticao.py

--- repeticao.py
+++ repeticao.py
@@ -1,6 +1,6 @@
 
 
-#For
+#For decrescente
 for andar in range(20, 0, -1):
     if andar != 13:
         print(andar)
@@ -8,7 +8,22 @@ for andar in range(20, 0, -1):
 
 
 
-#while
+# Usando o loop 'for' em ordem crescente
+for andar in range(1, 21):
+    if andar != 13:
+        print(andar)
+
+
+# Usando o loop 'while' em ordem crescente
+andar = 1
+while andar <= 20:
+    if andar != 13:
+        print(andar)
+    andar += 1
+
+
+
+#while o loop em ordem decrescente
 
 andar = 20
 while andar >= 1:
